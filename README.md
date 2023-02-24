9b0d594cedd7d75cac22e758c91a5228d29b2759
Ignacio Ribes <ignacioribes@gmail.com>
2023-02-24 11:36:04 -0300

    some improves

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Actualizar precios de todos los productos automaticamente cada mes
 
 Al instalar el plugin se actualizan automaticamente todos los precios de los productos un 5% por mes.
+
+Para ver las opciones del plugin, deberías ir a la página "Configuración > General" en el panel de administración de WordPress. Allí verás una nueva sección con el título "Actualizar precio de producto cada mes", que contiene el campo de entrada para el valor multiplicador que se usará para actualizar los precios de los productos cada mes.
+
+Preguntas frecuentes
+
+¿La actualizacion se hace el primer dia del mes?
+Sí, la actualización se hará cada mes en el primer día del mes. Esto se debe a que en la línea de código que programó la tarea cron, se establece la frecuencia de ejecución en 'monthly', lo que significa que la tarea se ejecutará una vez al mes, y la hora de inicio se establece en la hora actual en la que se activa el plugin mediante la función wp_schedule_event( time(), 'monthly', 'update_product_prices_cron' );.
+
+Entonces, si activas el plugin el día 10 del mes, la primera actualización de precios se realizará en el primer día del siguiente mes y así sucesivamente. Si deseas cambiar la fecha de actualización, puedes modificar el código para que se ejecute en un día específico del mes. Por ejemplo, si deseas que la actualización se realice el día 15 de cada mes, puedes cambiar el código a wp_schedule_event( strtotime('15th day of every month'), 'monthly', 'update_product_prices_cron' );.
--- plugin-actualizar-precios-woocommerce.php
+++ plugin-actualizar-precios-woocommerce.php
@@ -3,12 +3,13 @@
 Plugin Name: Actualización de precios mensual
 Description: Actualiza los precios de los productos cada mes automáticamente.
 Version: 1.0
-Author: Tu nombre
-Author URI: Tu URL
+Author: Ignacio Ribes
+Author URI: https://github.com/ignacioribes/
 */
 
 // Crea una nueva tarea cron para actualizar los precios cada mes
 function update_product_prices() {
+    $value_multiplier = get_option( 'update_product_prices_multiplier', 1.05 );
     $args = array(
         'post_type' => 'product',
         'posts_per_page' => -1
@@ -17,8 +18,6 @@ function update_product_prices() {
     if ( $loop->have_posts() ) {
         while ( $loop->have_posts() ) : $loop->the_post();
             global $product;
-
-            $value_multiplier = 1.05;
 
             $regular_price = $product->get_regular_price();
             $update_regular_price = $regular_price * $value_multiplier;
@@ -43,4 +42,36 @@ function schedule_product_price_update() {
     }
 }
 add_action( 'wp', 'schedule_product_price_update' );
+
+// Agrega la opción de configuración para actualizar el valor multiplicador
+function update_product_prices_settings() {
+    add_settings_section(
+        'update_product_prices_settings_section',
+        'Actualizar precio de producto cada mes',
+        'update_product_prices_settings_section_callback',
+        'general'
+    );
+
+    add_settings_field(
+        'update_product_prices_multiplier',
+        'Valor multiplicador para actualizar precios',
+        'update_product_prices_multiplier_callback',
+        'general',
+        'update_product_prices_settings_section'
+    );
+
+    register_setting( 'general', 'update_product_prices_multiplier' );
+}
+add_action( 'admin_menu', 'update_product_prices_settings' );
+
+// Renderiza el texto de la sección de configuración
+function update_product_prices_settings_section_callback() {
+    echo 'Configura el valor multiplicador que se usará para actualizar los precios de los productos cada mes.';
+}
+
+// Renderiza el campo de entrada de la opción de configuración
+function update_product_prices_multiplier_callback() {
+    $value_multiplier = get_option( 'update_product_prices_multiplier', 1.05 );
+    echo '<input type="number" step="0.01" min="0" id="update_product_prices_multiplier" name="update_product_prices_multiplier" value="' . $value_multiplier . '" />';
+}
 ?>
